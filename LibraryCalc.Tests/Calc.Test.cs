3f0d1ed8ad63ef825809ef458499f5e00ca01a3b
Valentina Falucca <valentina.falucca@hotmail.com>
2018-01-29 21:54:05 +0100

    Creazione primo metodo e primo test

---
+++ LibraryCalc.Tests/Calc.Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibraryCalc;
+
+namespace LibraryCalc.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSomma()
+        {
+            int a = 2;
+            int b = 3;
+            int somma=Calc.Somma(a, b);
+            Assert.AreEqual(6,somma);
+
+        }
+    }
+}
---
+++ LibraryCalc/Calc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCalc
+{
+  public class Calc
+    {
+
+       public static int Somma (int a, int b)
+            {int somma = 0;
+            somma=a+b;
+            return somma;
+            }
+            }
+
+
+    
+}
